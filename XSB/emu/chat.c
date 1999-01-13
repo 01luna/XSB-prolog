547dff0474a079bba17cc9125794b21fd33f5fc3
kostis <kostis@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
1999-01-13 20:28:27 +0000

    Changes to fix statistics.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@333 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/chat.c
+++ XSB/emu/chat.c
@@ -18,7 +18,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: chat.c,v 1.1 1998-12-21 01:07:58 cbaoqiu Exp $
+** $Id: chat.c,v 1.2 1999-01-13 20:28:26 kostis Exp $
 ** 
 */
 
@@ -963,11 +963,11 @@ void print_chat_statistics(void)
 {
   printf("  total size CHAT areas: %d b; high water mark: %d b; not freed: %d b\n",
 	  chat_total_malloced,chat_malloc_high_mark,chat_inuse);
-  printf("  consumers saved: %d; increments saved: %d; shared increments: %d\n",
+  printf("  suspensions saved: %d; increments saved: %d; shared increments: %d\n",
 	  chat_number_saved_consumers,
 	  chat_number_incremental_saves,
 	  chat_area_sharing);
-  printf("  number of restored consumers: %d; total restored memory: %d\n",
+  printf("  number of restored suspensions: %d; total restored memory: %d\n",
 	 chat_nr_of_restores,chat_restored_memory);
 }
 
