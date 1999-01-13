22e9f81a926dff1d67028977f036837b423ecb7c
kifer <kifer@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
1999-01-13 17:36:27 +0000

    deleted the first arg; added checks
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@329 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/supertest.sh
+++ xsbtests/supertest.sh
@@ -1,20 +1,22 @@
 #! /bin/sh
 
-# $1 must be the XSB to test
-# $2 must be the path of the XSB installation directory
+# $1 must be the path to the XSB installation directory
 # This script expects to be run from the testsuite directory
 
-XEMU=$1
-xsbdir=$2
+xsbdir=$1
 testdir=`pwd`
 logfile=/tmp/xsb_super_log.`whoami`
 lockfile=lock.super
 
-test -x "$1" || \
- (echo "$1: XSB executable doesn't exist or has wrong permissions" ; exit)
-test -d "$2" || \
- (echo "$2: XSB root directory doesn't exist or has wrong permissions" ; exit)
+if test -z "$xsbdir" ; then
+   echo "Arg 1 must be a path to the XSB installation directory"
+   exit 1
+elif test ! -d "$xsbdir" ; then
+   echo "$xsbdir: XSB root directory doesn't exist or has wrong permissions"
+   exit 1
+fi
 
+# Make sure to delete locks, if the user decides to abort the test
 trap 'rm -fr $testdir/$lockfile $testdir/lock.test; exit 1' 1 2 15
 
 if test -f "$testdir/$lockfile" ; then
@@ -25,34 +27,34 @@ else
 fi
 
 cd $xsbdir/build
-echo "Configuring $XEMU with default options"
+echo "Configuring XSB with default options"
 configure > $logfile
-echo "Making $XEMU with default options"
+echo "Making XSB with default options"
 makexsb fast >> $logfile
 cd $testdir
 ./testsuite.sh $2/bin/xsb
 
 cd $xsbdir/build
-echo "Configuring $XEMU with --enable-local-scheduling"
+echo "Configuring XSB with --enable-local-scheduling"
 configure --config-tag=localsched --enable-local-scheduling >> $logfile
-echo "Making $XEMU with --enable-local-scheduling"
+echo "Making XSB with --enable-local-scheduling"
 makexsb --config-tag=localsched fast >> $logfile
 cd $testdir
 ./testsuite.sh $2/bin/xsb-localsched
 
 cd $xsbdir/build
-echo "Configuring $XEMU with --enable-chat"
+echo "Configuring XSB with --enable-chat"
 configure --config-tag=chat --enable-chat >> $logfile
-echo "Making $XEMU with --enable-chat"
+echo "Making XSB with --enable-chat"
 makexsb --config-tag=chat fast >> $logfile
 cd $testdir
 ./testsuite.sh $2/bin/xsb-chat
 
 
 cd $xsbdir/build
-echo "Configuring $XEMU with --enable-chat --enable-local-scheduling"
+echo "Configuring XSB with --enable-chat --enable-local-scheduling"
 configure --config-tag=localschedNchat --enable-chat --enable-local-scheduling >> $logfile
-echo "Making $XEMU with --enable-chat --enable-local-scheduling"
+echo "Making XSB with --enable-chat --enable-local-scheduling"
 makexsb --config-tag=localschedNchat fast >> $logfile
 cd $testdir
 ./testsuite.sh $2/bin/xsb-localschedNchat
