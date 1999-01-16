a51c6fd7f1e3255375fb20d65eaac56d83db3c03
kostis <kostis@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
1999-01-16 16:30:05 +0000

    Small changes to use file "ptq_out" for outputting the tables.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@355 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/ptq/genlredscaff.sh
+++ xsbtests/ptq/genlredscaff.sh
@@ -8,14 +8,13 @@ for suff in $suffixes ; do
     test -f "$file" &&  /bin/rm -f $file
 done
 
-$XEMU -m 4000 -c 2000 -i << EOF
-[ptqlred].
-import output_table/2 from tables.
+$XEMU << EOF
+[ptqlred,ptq_out].
 $1.
-output_table(s(_,_,_,_,_),$2_s).
-output_table(te1(_,_,_,_,_,_,_),$2_te1).
-output_table(cn(_,_,_,_,_,_),$2_cn).
-output_table(iv(_,_,_,_,_,_),$2_iv).
+output_table($2_s,s(_,_,_,_,_)).
+output_table($2_te1,te1(_,_,_,_,_,_,_)).
+output_table($2_cn,cn(_,_,_,_,_,_)).
+output_table($2_iv,iv(_,_,_,_,_,_)).
 halt.
 EOF
 
