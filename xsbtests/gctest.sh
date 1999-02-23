fbdc7e67c39dad3c6f43c634ee94e8de7cfe48d7
kostis <kostis@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
1999-02-23 17:54:02 +0000

    Tests to ensure that the garbage collector works.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@472 756869e0-9e47-4bc5-b0a8-847305dc5e1c

---
+++ xsbtests/gctest.sh
@@ -0,0 +1,74 @@
+#! /bin/sh
+
+## File:      gctest.sh
+## Author(s): Kostis Sagonas
+## Contact:   xsb-contact@cs.sunysb.edu
+## 
+## Copyright (C) K.U. Leuven, 1999
+## 
+## XSB is free software; you can redistribute it and/or modify it under the
+## terms of the GNU Library General Public License as published by the Free
+## Software Foundation; either version 2 of the License, or (at your option)
+## any later version.
+## 
+## XSB is distributed in the hope that it will be useful, but WITHOUT ANY
+## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+## FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for
+## more details.
+## 
+## You should have received a copy of the GNU Library General Public License
+## along with XSB; if not, write to the Free Software Foundation,
+## Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+##
+## $Id: gctest.sh,v 1.1 1999-02-23 17:54:00 kostis Exp $
+## 
+##
+
+# THIS SCRIPT EXPECTS TO BE RUN FROM THE TESTSUITE DIRECTORY
+
+# $1 must be the path to the XSB installation directory
+
+xsbdir=$1
+testdir=`pwd`
+logfile=/tmp/xsb_gc_log.`whoami`
+lockfile=lock.gc
+
+if test -z "$xsbdir" ; then
+   echo "Arg 1 must be a path to the XSB installation directory"
+   exit 1
+elif test ! -d "$xsbdir" ; then
+   echo "$xsbdir: XSB root directory doesn't exist or has wrong permissions"
+   exit 1
+fi
+
+# Make sure to delete locks, if the user decides to abort the test
+trap 'rm -fr $testdir/$lockfile $testdir/lock.test; exit 1' 1 2 15
+
+if test -f "$testdir/$lockfile" ; then
+ echo "./$lockfile exists. Remove it first"
+ exit
+else
+ echo $$ > $lockfile
+fi
+
+#------------------------------------------------------------------
+
+cd $xsbdir/build
+echo "Configuring XSB with --enable-chat --enable-gc-test"
+configure --config-tag=chatgc --enable-chat --enable-gc-test >> $logfile
+echo "Making XSB with --enable-chat --enable-gc-test"
+makexsb --config-tag=chatgc fast >> $logfile
+cd $testdir
+./testsuite.sh $xsbdir chatgc ./gctestall.sh
+
+cd $xsbdir/build
+echo "Configuring XSB with --enable-chat --enable-local-scheduling --enable-gc-test"
+configure --config-tag=chatlocalgc --enable-chat --enable-local-scheduling --enable-gc-test >> $logfile
+echo "Making XSB with --enable-chat --enable-local-scheduling --enable-gc-test"
+makexsb --config-tag=chatlocalgc fast >> $logfile
+cd $testdir
+./testsuite.sh $xsbdir chatlocalgc ./gctestall.sh
+
+#------------------------------------------------------------------
+
+rm $testdir/$lockfile
