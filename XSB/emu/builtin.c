414a8035d52e14be2d863420573fb43c89cd6228
kifer <kifer@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
1999-04-02 20:16:59 +0000

    Added comments, to help me through
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@566 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/builtin.c
+++ XSB/emu/builtin.c
@@ -19,7 +19,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: builtin.c,v 1.30 1999-04-02 03:48:01 kifer Exp $
+** $Id: builtin.c,v 1.31 1999-04-02 20:16:59 kifer Exp $
 ** 
 */
 
@@ -244,6 +244,7 @@ int readmsg(SOCKET sockfd, char *buff, int maxbuff)
  */
 Cell ptoc_tag(int regnum)
 {
+  /* reg is global array in register.h */
   register Cell addr = cell(reg+regnum);
 
   deref(addr);
@@ -253,6 +254,7 @@ Cell ptoc_tag(int regnum)
 
 DllExport prolog_int call_conv ptoc_int(int regnum)
 {
+  /* reg is global array in register.h */
   register Cell addr = cell(reg+regnum);
 
   /* deref and then check the type */
@@ -273,6 +275,7 @@ DllExport prolog_int call_conv ptoc_int(int regnum)
 
 DllExport prolog_float call_conv ptoc_float(int regnum)
 {
+  /* reg is global array in register.h */
   register Cell addr = cell(reg+regnum);
 
   /* deref and then check the type */
@@ -293,6 +296,7 @@ DllExport prolog_float call_conv ptoc_float(int regnum)
 
 DllExport char* call_conv ptoc_string(int regnum)
 {
+  /* reg is global array in register.h */
   register Cell addr = cell(reg+regnum);
   
   /* deref and then check the type */
@@ -339,6 +343,7 @@ DllExport void call_conv ctop_int(int regnum, prolog_int value)
 
 DllExport void call_conv ctop_float(int regnum, prolog_float value) /* from float value form an int node */
 {
+  /* reg is global array in register.h */
   register Cell addr = cell(reg+regnum);
 
   deref(addr);
@@ -348,8 +353,10 @@ DllExport void call_conv ctop_float(int regnum, prolog_float value) /* from floa
   else fprintf(stderr, "Wrong arg in ctop_float %lux\n", addr);
 }
 
-DllExport void call_conv ctop_string(int regnum, char *value) /* from string form a string node */
+/* take a C string, form a string node */
+DllExport void call_conv ctop_string(int regnum, char *value)
 {
+  /* reg is global array in register.h */
   register Cell addr = cell(reg+regnum);
 
   deref(addr);
