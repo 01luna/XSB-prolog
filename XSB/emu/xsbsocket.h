db21903b4fbbf08552a888814b9d4f4a55eefc7f
kifer <kifer@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
1999-08-02 00:29:00 +0000

    renamed xsbsockets.i to xsbsockets.c added socket_set-option
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@1039 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/xsbsocket.h
+++ XSB/emu/xsbsocket.h
@@ -18,7 +18,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: xsbsocket.h,v 1.7 1999-07-24 23:33:30 kifer Exp $
+** $Id: xsbsocket.h,v 1.8 1999-08-02 00:28:59 kifer Exp $
 ** 
 */
 
@@ -28,7 +28,7 @@
 #define SOCKET_LISTEN      2
 #define SOCKET_ACCEPT      3
 #define SOCKET_CONNECT     4
-#define SOCKET_FLUSH       5
+/* #define SOCKET_FLUSH       5  deleted, use file_function */
 #define SOCKET_CLOSE       6
 #define SOCKET_RECV	   7
 #define SOCKET_SEND	   8
@@ -36,6 +36,7 @@
 #define SOCKET_SEND_ASCI   10
 #define SOCKET_GET0        11
 #define SOCKET_PUT         12
+#define SOCKET_SET_OPTION  13
 
 #ifdef WIN_NT
 #define IPPROTO_TCP                0  /* defined in Unix, but not Windows */
