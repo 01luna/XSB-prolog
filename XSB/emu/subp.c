c91d3c3d2cccfdfa6a7a2543ce1d659bf02c8087
kostis <kostis@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
1999-08-06 14:35:50 +0000

    Some more performance-related changes to the unification routine.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@1056 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/subp.c
+++ XSB/emu/subp.c
@@ -19,7 +19,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: subp.c,v 1.13 1999/07/22 15:24:12 warren Exp $
+** $Id: subp.c,v 1.14 1999/08/04 16:21:36 kifer Exp $
 ** 
 */
 
@@ -85,7 +85,7 @@ void (*xsb_default_segfault_handler)(int); /* where the previous value of the
 bool unify(Cell rop1, Cell rop2)
 { /* begin unify */
   register Cell op1, op2;
-  long arity, i;
+  long arity;
 
   op1 = rop1; op2 = rop2;
 
@@ -291,7 +291,7 @@ Float asfloat(Cell w)
   return float_conv.f;
 }
 
-static int sign(Float num)
+static inline int sign(Float num)
 {
   if (num==0.0) return 0;
   else if (num>0.0) return 1;
