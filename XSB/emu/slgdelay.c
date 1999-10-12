2aa421fbeb42e8394c44d350cd4134da7a9d8683
ejohnson <ejohnson@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
1999-10-12 20:25:09 +0000

    Comment change (updated mentioned function name).
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@1339 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/slgdelay.c
+++ XSB/emu/slgdelay.c
@@ -19,7 +19,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: slgdelay.c,v 1.18 1999/09/18 04:39:44 cbaoqiu Exp $
+** $Id: slgdelay.c,v 1.19 1999/10/05 04:01:53 kifer Exp $
 ** 
 */
 
@@ -378,7 +378,7 @@ static void record_de_usage(DL dl)
  * Function do_delay_stuff() is called in the SLG instruction
  * `new_answer_dealloc', when an answer (new or not) is returned for the
  * current call.  Here, `as_leaf' is the leaf node of the answer trie
- * (TrieRetPtr, the return value of variant_trie_search), `subgoal' is
+ * (TrieRetPtr, the return value of variant_answer_search), `subgoal' is
  * the subgoal frame of the current call, and `sf_exists' tells whether
  * this answer is new or not.
  *
