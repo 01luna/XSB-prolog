ca9dee098a847610fa998a5b9eb8ea5757e0567c
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
1999-10-23 21:18:00 +0000

    slowly, slowly making a sophisticated benchmarking suite.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@1430 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/benches/synth_benches/test.sh
+++ xsbtests/benches/synth_benches/test.sh
@@ -1,18 +1,14 @@
 #! /bin/sh
 
-echo "-------------------------------------------------------"
-echo "--- Running benches/def_tests/test.sh               ---"
-echo "-------------------------------------------------------"
-
 XEMU=$1
 options=$2
 
-$XEMU $options << EOF
-
-EOF
+echo "-------------------------------------------------------"
+echo "--- Running benches/synth_benches/test.sh           ---"
+echo "-------------------------------------------------------"
 
 # XEMU and options must be together in quotes
-#../genbench.sh "$XEMU $options"  "load_dync(chain),[anc,bench]." "chain_ancestor_bench."
-#../genbench.sh "$XEMU $options"  "load_dync(cycle),[anc,bench]." "cycle_ancestor_bench."
+../genbench.sh "$XEMU $options"  "load_dync(chain),[anc,bench]." "chain_ancestor_bench."
+../genbench.sh "$XEMU $options"  "load_dync(cycle),[anc,bench]." "cycle_ancestor_bench."
 ../genbench.sh "$XEMU $options"  "load_dync(cycle),[win,bench]." "cycle_win_bench."
 ../genbench.sh "$XEMU $options"  "load_dync(chain),[win,bench]." "chain_win_bench."
