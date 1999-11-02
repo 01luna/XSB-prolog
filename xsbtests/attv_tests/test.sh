ed16fab31da815f82f3fdc8539f2d4bbedeb3b44
cbaoqiu <cbaoqiu@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
1999-11-02 02:48:59 +0000

    Added a new test for attv.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@1504 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/attv_tests/test.sh
+++ xsbtests/attv_tests/test.sh
@@ -11,3 +11,4 @@ opts=$2
 ../gentest.sh "$XEMU $opts" attv_test "test."
 ../gentest.sh "$XEMU $opts" tabled_attv "test."
 ../gentest.sh "$XEMU $opts" interrupt1 "test."
+../gentest.sh "$XEMU $opts" interrupt2 "test."
