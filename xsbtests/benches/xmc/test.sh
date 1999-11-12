7fb3719e242a0cf40ec16ce483eaf208c9898456
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
1999-11-12 20:51:54 +0000

    Modified to run with new copy of precise_bench
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@1545 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/benches/xmc/test.sh
+++ xsbtests/benches/xmc/test.sh
@@ -5,9 +5,8 @@ echo "--- Running benches/xmc/test.sh                     ---"
 echo "-------------------------------------------------------"
 
 XEMU=$1
-options=$2
-
+FILE=$2
 
 # XEMU and options must be together in quotes
-../genbench.sh "$XEMU $options"  "[main]." "bench_leader."
-../genbench.sh "$XEMU $options"  "[main]." "bench_sieve."
+../genbench.sh "$XEMU"  "[main]." "bench_leader." "$FILE"
+../genbench.sh "$XEMU"  "[main]." "bench_sieve." "$FILE"
