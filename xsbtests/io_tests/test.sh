aa75a62b731e542cdd98c2a9ee675338e7789225
cbaoqiu <cbaoqiu@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2000-02-12 22:45:37 +0000

    Added back with execute permission.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@1769 756869e0-9e47-4bc5-b0a8-847305dc5e1c

---
+++ xsbtests/io_tests/test.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+echo "-------------------------------------------------------"
+echo "--- Running io_tests/test.sh                        ---"
+echo "-------------------------------------------------------"
+
+XEMU=$1
+options=$2
+
+# XEMU and options must be together in quotes
+../gentest.sh "$XEMU $options" io_basics "test."
+../gentest.sh "$XEMU $options" fmt_io "test."
+#../gentest.sh "$XEMU $options" io_timeout "test."
