7a1fc2a80914d795567143c0a6d37b2663ce736f
kifer <kifer@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2001-02-20 21:49:44 +0000

    added cwd/1 (current working dir) documented other syscalls in shell.P
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@2218 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/syscall_xsb.h
+++ XSB/emu/syscall_xsb.h
@@ -18,7 +18,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: syscall_xsb.h,v 1.1 1999-10-25 05:59:33 kifer Exp $
+** $Id: syscall_xsb.h,v 1.2 2001-02-20 21:49:43 kifer Exp $
 ** 
 */
 
@@ -208,3 +208,7 @@
 #define SYS_fchdir	176
 #define SYS_fchroot	177
 #define SYS_vpixsys	178
+
+/* XSB-specific numbers */
+
+#define SYS_cwd         300  /* current working directory */
