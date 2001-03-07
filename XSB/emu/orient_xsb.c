66a4753dd0ee715b80bc2f97ec63b5d65a97f1bb
dwarren <dwarren@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2001-03-07 17:22:05 +0000

    made cygwin/windows more compatible wrt filenames.  Now for windows users, all forward slashes are converted to backslashes in filenames.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@2249 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/orient_xsb.c
+++ XSB/emu/orient_xsb.c
@@ -18,7 +18,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: orient_xsb.c,v 1.9 2001-03-07 15:29:08 kifer Exp $
+** $Id: orient_xsb.c,v 1.10 2001-03-07 17:22:05 dwarren Exp $
 ** 
 */
 
@@ -61,7 +61,7 @@ DllExport extern char * call_conv strip_names_from_path(char*, int);
 
 static void check_create_dir(char *);
 
-extern void transform_pathname(char *);
+extern void transform_cygwin_pathname(char *);
 
 char current_dir[MAXPATHLEN];
 char xsbinfo_dir[MAXPATHLEN];
@@ -152,12 +152,12 @@ char *xsb_executable_full_path(char *myname)
 #ifdef WIN_NT
   /* CygWin32 uses absolute paths like this:
      //<drive letter>/dir1/dir2/...
+     actually /cygdrive/<drive letter>/....
      If we find such a path, we transform it to a windows-like pathname.
      This assumes that XSB has been compiled using the native Windows
      API, and is being run from CygWin32 bash (like from the test
      scripts). */
-  if (myname_augmented[0] == '/' && myname_augmented[1] == '/') 
-    transform_pathname(myname_augmented);
+  transform_cygwin_pathname(myname_augmented);
 #endif
 
   if (is_absolute_filename(myname_augmented))
