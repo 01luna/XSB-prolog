586701367ca02c5c2a5ae30527eb95405a4654f1
ejohnson <ejohnson@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2001-04-29 17:21:13 +0000

    Small change to debugging output.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@2381 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/tst_utils.c
+++ XSB/emu/tst_utils.c
@@ -18,7 +18,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: tst_utils.c,v 1.19 2001-04-28 20:15:38 ejohnson Exp $
+** $Id: tst_utils.c,v 1.20 2001-04-29 17:21:13 ejohnson Exp $
 ** 
 */
 
@@ -185,6 +185,8 @@ void printTrieSymbol(FILE *fp, Cell symbol) {
 void printTrieNode(FILE *fp, BTNptr pTN) {
 
   fprintf(fp, "Trie Node: Addr(%p)", pTN);
+  if ( IsDeletedNode(pTN) )
+    fprintf(fp, "  (DELETED)");
   fprintf(fp, "\n\tInstr(%s), Status(%s), NodeType(%s),\n"
 	  "\tTrieType(%s), Symbol(",
 	  inst_name(TN_Instr(pTN)),
@@ -252,7 +254,7 @@ static void symstkPrintNextTerm(FILE *fp, xsbBool list_recursion) {
       Psc psc;
       int i;
 
-      if ( list_recursion )
+b      if ( list_recursion )
 	fprintf(fp, "|");
       psc = DecodeTrieFunctor(symbol);
       fprintf(fp, "%s(", get_name(psc));
