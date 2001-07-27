b859c7bcf24ff66ab23961eb4620be4d09680f1f
dwarren <dwarren@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2001-07-27 13:27:02 +0000

    Changed signed shift of addrs to unsigned shift to save one more bit in addresses, for BIG_MEM tagging.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@2615 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/cell_xsb.h
+++ XSB/emu/cell_xsb.h
@@ -18,7 +18,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: cell_xsb.h,v 1.11 2001-02-06 16:56:36 lfcastro Exp $
+** $Id: cell_xsb.h,v 1.12 2001-07-27 13:27:02 dwarren Exp $
 ** 
 */
 
@@ -80,8 +80,10 @@ typedef Cell *CPtr;
 
 #ifdef BITS64
 typedef long Integer ;
+typedef unsigned long UInteger ;
 #else
 typedef int Integer ;
+typedef unsigned int UInteger ;
 #endif
  
 #ifdef BITS64
@@ -171,10 +173,10 @@ extern Float getfloatval(Cell);
 
 #define enc_int(val) ( ((Integer)(val) & 0xc0000003) ?\
 		       (((Integer)(val) << 4) | 0x08) :\
-		       ((Integer)(val) << 2) )
+		       ((UInteger)(val) << 2) )
 #define dec_int(val) ( ((Integer)(val) & 0x08) ?\
 		       ((Integer)(val) >> 4) :\
-		       (((Integer)(val) >> 2) & 0x3ffffffc) )
+		       (((UInteger)(val) >> 2) & 0x3ffffffc) )
 
 #define enc_addr(addr) ((Cell)(addr) << 1)
 #define dec_addr(dcell) (((Cell)(dcell) >> 1) & 0x7ffffffc)
