14c4d063b566a56edf0770ac83cf82b4fad5c6f9
kifer <kifer@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2002-03-14 09:55:05 +0000

    use variables instead of explicit .O in makefiles
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@2863 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/loader_defs.h
+++ XSB/emu/loader_defs.h
@@ -18,11 +18,11 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: loader_defs.h,v 1.1 2002-03-13 10:17:47 kifer Exp $
+** $Id: loader_defs.h,v 1.2 2002-03-14 09:55:03 kifer Exp $
 ** 
 */
 
-/* Info on the file extension used for XSB object files */
+/* Info on the file extension used for XSB files */
 
 #define XSB_OBJ_EXTENSION_STRING  ".O"
 #define XSB_OBJ_EXTENSION_LENGTH  4
