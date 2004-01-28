dc3b8bce3ce00b5f87865dbba305e68ded8d5e0c
dwarren <dwarren@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2004-01-28 20:28:07 +0000

    Add check for profiling interrupt in trust-type instructions. Gives better profiling feedback and single bit-check there should be quite cheap.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@3765 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/emuloop.c
+++ XSB/emu/emuloop.c
@@ -19,7 +19,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: emuloop.c,v 1.97 2004-01-16 22:03:56 dwarren Exp $
+** $Id: emuloop.c,v 1.98 2004-01-28 20:28:07 dwarren Exp $
 ** 
 */
 
@@ -804,6 +804,7 @@ contcase:     /* the main loop */
     Def1op
     Op1(get_xxa);
     restore_type = 1;
+    handle_xsb_profile_interrupt;
     ADVANCE_PC(size_xxx);
     RESTORE_SUB
   XSB_End_Instr()
@@ -838,6 +839,7 @@ contcase:     /* the main loop */
   XSB_Start_Instr(trust,_trust) /* PPA-L */
     Def1op
     Op1(get_xxa);
+    handle_xsb_profile_interrupt;
     lpcreg = *(pb *)(lpcreg+sizeof(Cell));
     restore_type = 1;
     RESTORE_SUB
@@ -1100,6 +1102,7 @@ contcase:     /* the main loop */
   XSB_Start_Instr(trustmeorelsefail,_trustmeorelsefail) /* PPA */
     Def1op
     Op1(0);
+    handle_xsb_profile_interrupt;
     ADVANCE_PC(size_xxx);
     cpreg = lpcreg+sizeof(Cell);
     restore_type = 1;
@@ -1109,6 +1112,7 @@ contcase:     /* the main loop */
   XSB_Start_Instr(dyntrustmeelsefail,_dyntrustmeelsefail) /* PPA-L, second word ignored */
     Def1op
     Op1(get_xxa);
+    handle_xsb_profile_interrupt;
     ADVANCE_PC(size_xxxX);
     restore_type = 1;
     RESTORE_SUB
