aec5c472b654243ebde2112fb81212e3fe40dbfc
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2004-02-17 23:39:02 +0000

    adding query tests... continued
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@3793 756869e0-9e47-4bc5-b0a8-847305dc5e1c

---
+++ xsbtests/cdf_tests/test.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+echo "-------------------------------------------------------"
+echo "--- Running cdf_tests/test.sh                     ---"
+echo "-------------------------------------------------------"
+
+XEMU=$1
+options=$2
+
+# XEMU and options must be together in quotes
+#------------------------------------
+# Tests of theorem prover
+#------------------------------------
+../loadtest.sh "$XEMU $options" ce_test "testall."
+../loadtest.sh "$XEMU $options" cdftp_test "oneclicktest."
+../gentest.sh "$XEMU $options" type0_test "testreturns."
