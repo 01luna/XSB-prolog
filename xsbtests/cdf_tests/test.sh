3146656a823cf425a13ddca8d536f30981ad0014
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2004-03-19 01:17:49 +0000

    Updates to checks for proper input.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@3853 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/cdf_tests/test.sh
+++ xsbtests/cdf_tests/test.sh
@@ -13,10 +13,39 @@ options=$2
 #------------------------------------
 ../loadtest.sh "$XEMU $options" ce_test "testall."
 ../loadtest.sh "$XEMU $options" cdftp_test "oneclicktest."
+
+#------------------------------------
+# Tests of Type 0 interface
+#------------------------------------
 ../gentest.sh "$XEMU $options" type0QueryExt "testreturns."
 ../gentest.sh "$XEMU $options" type0QueryInt "testreturns."
 ../gentest.sh "$XEMU $options" dlQuery "testreturns."
+
+#------------------------------------
+# Tests of Updates
+#------------------------------------
 ../gentest.sh "$XEMU $options" updateExt "testreturns."
 ../gentest.sh "$XEMU $options" updateInt "testreturns."
 ../gentest.sh "$XEMU $options" updateDL "testreturns."
+
+#------------------------------------
+# Tests of checks
+#------------------------------------
 ../gentest.sh "$XEMU $options" checks "testreturns."
+
+#------------------------------------
+# Tests of I/O
+#------------------------------------
+./exttest.sh "$XEMU $options" ioExt "testreturns."
+# inttest doesn't sort 
+./inttest.sh "$XEMU $options" ioInt "testreturns."
+
+#------------------------------------
+# Component Tests
+#------------------------------------
+# Test creating a new component.
+./compUpd1.sh "$XEMU $options"
+# Load newly created component and update in place.
+./compUpd2.sh "$XEMU $options"
+# Test merge
+./compUpd3.sh "$XEMU $options"
