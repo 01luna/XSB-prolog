ebdfd582a49b18bd5939b06f367a9680085587b5
ruim <ruim@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2005-07-04 17:46:27 +0000

    added test for sharing completed tables
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@4293 756869e0-9e47-4bc5-b0a8-847305dc5e1c

---
+++ mttests/shared_comp_tests/test.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+XEMU=$1
+NUM=$2
+options=$3
+
+echo "---------------------------------------------------------"
+echo "--- Running shared_comp_tests/test.sh w. $NUM threads ---"
+echo "---------------------------------------------------------"
+
+#------------------------------------
+# tests involving definite tabling
+#------------------------------------
+# XEMU and options must be together in quotes
+# convention is that to use test_single, leave the last argument of test
+# goal uninstantiated to unify with the stream.
+
+../gentest.sh "$XEMU $options" sharedcomp1 "sc1(1000,_)" $NUM
+
+
