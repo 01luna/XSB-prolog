5cdf5e9b3f0742807ccd4b7c19e8f9443fb4f087
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2005-11-08 01:05:16 +0000

    Documentation changes only -- how attvs are traversed in variant_call_search and the setup for using trie instructions on completed tables.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@4528 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/tries.c
+++ XSB/emu/tries.c
@@ -20,7 +20,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: tries.c,v 1.75 2005-11-01 00:36:20 tswift Exp $
+** $Id: tries.c,v 1.76 2005-11-08 01:05:16 tswift Exp $
 ** 
 */
 
@@ -794,10 +794,16 @@ BTNptr variant_answer_search(CTXTdeclc int sf_size, int attv_num, CPtr cptr,
    * will generate a trie_xxx_attv instruction.  Note that in doing
    * this, attributes in the call will not need to be re-entered in
    * the table.
-   *
-   * To save time, this is only done when there is at least one attv in
-   * the call (attv_num > 0).
-   * TLS: probably only need to inc counter for shared attvs.
+   * 
+   * According to Bao's algorithm, in order for trie instructions for
+   * completed tables to work for attvs, attvs in the call must be
+   * traversed before the main loop and bound to elements of
+   * varEnumerator so that the trie_xxx_val instructions can recognize
+   * them and avoid interrupts.  As a result, both here and in the tabletry
+   * setup for completed tables, the substitution factor is traversed
+   * and the attvs set to the lower portion of varEnumerator.  To save
+   * time, this is only done when there is at least one attv in 
+   * the call (attv_num > 0).  ¹
    */
   if (attv_num > 0) {
     for (i = 0; i < sf_size; i++) {
