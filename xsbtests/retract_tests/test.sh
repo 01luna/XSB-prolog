c47ce7bab0b895910126b0aef0efe877d56ff96f
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2006-04-27 00:53:26 +0000

    Changes to support efficient marking and unmarking in retractall when called with a non-open term.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@4765 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/retract_tests/test.sh
+++ xsbtests/retract_tests/test.sh
@@ -18,3 +18,17 @@ options=$2
 #---------------------------------------
 ../gentest.sh "$XEMU $options" load_backsuite "test."
 #---------------------------------------
+../gentest.sh "$XEMU $options" test_retract_gc "test."
+#---------------------------------------
+../gentest.sh "$XEMU $options" test_retr_gc_noind "test."
+#---------------------------------------
+../gentest.sh "$XEMU $options" test_retr_gc_fancy "test."
+#---------------------------------------
+../gentest.sh "$XEMU $options" test_retra_gc "test."
+#---------------------------------------
+../gentest.sh "$XEMU $options" test_retra_gc_noind "test."
+#---------------------------------------
+../gentest.sh "$XEMU $options" test_retra_gc_fancy "test."
+#---------------------------------------
+../gentest.sh "$XEMU $options" test_retra_gc_nongen_fancy "test."
+#---------------------------------------
