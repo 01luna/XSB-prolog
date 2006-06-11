8209f6898fc069aa086b919a6e42d73105e79789
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2006-06-11 21:52:50 +0000

    updates to make C interface compatable with MT engine.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@4838 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/prolog_tests/xeddis.c
+++ xsbtests/prolog_tests/xeddis.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "xsb_config.h"
 #include "cinterf.h"
+#include "context.h"
 
 static int mymin(int a, int b, int c)
 {
@@ -13,15 +14,15 @@ static int mymin(int a, int b, int c)
   }
 }
 
-DllExport int call_conv min_ed_dis(void)
+DllExport int call_conv min_ed_dis(CTXTdecl)
 {
   int len1,len2, i, j;
   int ins,del,rep;
   int cost[200][200];
   char *string1, *string2;
 
-  string1 = ptoc_string(1);
-  string2 = ptoc_string(2);
+  string1 = extern_ptoc_string(1);
+  string2 = extern_ptoc_string(2);
   len1 = strlen(string1);
   len2 = strlen(string2);
   for (i = 0; i < 200; i++) cost[i][0]= i;
@@ -36,6 +37,6 @@ DllExport int call_conv min_ed_dis(void)
       cost[i][j] = mymin(del,ins,rep);
     }
   }   
-  ctop_int(3,cost[len1][len2]);
+  extern_ctop_int(3,cost[len1][len2]);
   return 1;
 }
--- xsbtests/prolog_tests/zero.c
+++ xsbtests/prolog_tests/zero.c
@@ -1,29 +1,30 @@
 #include "xsb_config.h"
 #include "cinterf.h"
+#include "context.h"
 
-DllExport int call_conv minus_one(void)
+DllExport int call_conv minus_one(CTXT)
 {
-  int i = ptoc_int(1);
-  ctop_int(2,i-1);
+  int i = extern_ptoc_int(1);
+  extern_ctop_int(2,i-1);
   return 1;
 }
 
-DllExport int call_conv zero(void)
+DllExport int call_conv zero(CTXT)
 {
-  ctop_int(1,0);
+  extern_ctop_int(1,0);
   return 1;
 }
 
-DllExport int call_conv plus_one(void)
+DllExport int call_conv plus_one(CTXT)
 {
-  int i = ptoc_int(1);
-  ctop_int(2,i+1);
+  int i = extern_ptoc_int(1);
+  extern_ctop_int(2,i+1);
   return 1;
 }
 
-DllExport int call_conv diag(void)
+DllExport int call_conv diag(CTXT)
 {
-  int n = ptoc_int(1);
-  ctop_int(2,n);
+  int n = extern_ptoc_int(1);
+  extern_ctop_int(2,n);
   return 1;
 }
