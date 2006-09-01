afaf08530738da611adffab2d4014b5dc96ae038
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2006-09-01 18:31:36 +0000

    Added test  chr libary, timeConstraints.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@4954 756869e0-9e47-4bc5-b0a8-847305dc5e1c

---
+++ xsbtests/constraint_tests/test.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+# This directory is intended for tests of Prolog-level constraints as
+# well as CHR libraries.  Attribute variable tests per se should be
+# in ../attv_tests.
+
+#============================================================================
+echo "-------------------------------------------------------"
+echo "--- Running constraint_tests/test.sh                ---"
+echo "-------------------------------------------------------"
+
+XEMU=$1
+opts=$2
+
+../gentest.sh "$XEMU $opts" test_constraintLib "test."
+../gentest.sh "$XEMU $opts" timetest "test."
