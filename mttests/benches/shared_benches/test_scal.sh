6ced3abefe39526b0c9de98af9a311b1137b7e26
ruim <ruim@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2006-11-07 00:28:03 +0000

    added scalability script
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@5044 756869e0-9e47-4bc5-b0a8-847305dc5e1c

---
+++ mttests/benches/shared_benches/test_scal.sh
@@ -0,0 +1,16 @@
+PREF=$1
+DIR=$2
+MAX=$3
+
+./make_graphs.sh $PREF/bin/xsb
+
+nthreads=1
+while [ $nthreads -le $MAX ]
+do
+	./mttest.sh $PREF/bin/xsb-mt \
+		    $DIR/shared-local.P $nthreads 
+	./mttest.sh $PREF/bin/xsb-btc-ccmpl \
+		    $DIR/shared-batched.P $nthreads "-c 8000"
+	nthreads=$[$nthreads*2]
+done
+
