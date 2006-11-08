4817416eec877d6a67b0e957c8f58ee03b6d847d
ruim <ruim@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2006-11-08 18:19:21 +0000

    undid the last change as, after all, CONC_COMPL needs wfs_xsb_i.h in some cases
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@5055 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/wfs_xsb_i.h
+++ XSB/emu/wfs_xsb_i.h
@@ -19,7 +19,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: wfs_xsb_i.h,v 1.17 2006-11-08 17:06:33 ruim Exp $
+** $Id: wfs_xsb_i.h,v 1.18 2006-11-08 18:19:21 ruim Exp $
 ** 
 */
 
@@ -376,7 +376,16 @@ static void batched_compute_wfs(CTXTdeclc CPtr leader_compl_frame,
      if (compl_visited(ComplStkFrame) != FALSE) {
        curr_subg = compl_subgoal_ptr(ComplStkFrame);
        if (compl_visited(ComplStkFrame) != DELAYED) {
-      	 mark_as_completed(curr_subg);
+#ifdef CONC_COMPL
+      if( !is_completed(curr_subg) )
+      {
+        mark_as_completed(curr_subg);
+	if( IsSharedSF( curr_subg ) )
+        	WakeDependentThreads(th, curr_subg);
+      }
+#else
+      mark_as_completed(curr_subg);
+#endif
 	 reclaim_incomplete_table_structs(curr_subg);
 	 if (neg_simplif_possible(curr_subg)) {
 	   simplify_neg_fails(CTXTc curr_subg);
@@ -502,7 +511,16 @@ static void batched_compute_wfs(CTXTdeclc CPtr leader_compl_frame,
     ComplStkFrame = leader_compl_frame;
     while (ComplStkFrame >= openreg) {
       curr_subg = compl_subgoal_ptr(ComplStkFrame);
+#ifdef CONC_COMPL
+      if( !is_completed(curr_subg) )
+      {
+        mark_as_completed(curr_subg);
+	if( IsSharedSF( curr_subg ) )
+        	WakeDependentThreads(th, curr_subg);
+      }
+#else
       mark_as_completed(curr_subg);
+#endif
       if (neg_simplif_possible(curr_subg)) {
 	simplify_neg_fails(CTXTc curr_subg);
       }
