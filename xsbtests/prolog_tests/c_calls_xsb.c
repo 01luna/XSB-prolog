e25ed753d53dccc3781250963ba907eccb2276a6
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2006-12-14 22:01:03 +0000

    Changes so that the c_calling_xsb.c test now works for the MT engine as well as the sequential engine.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@5144 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/prolog_tests/c_calls_xsb.c
+++ xsbtests/prolog_tests/c_calls_xsb.c
@@ -1,10 +1,13 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /* The following include is necessary to get the macros and routine
    headers */
 
 #include "cinterf.h"
+#include "context.h"
+
 extern char *xsb_executable_full_path(char *);
 extern char *strip_names_from_path(char*, int);
 
@@ -13,6 +16,13 @@ int main(int argc, char *argv[])
   int rcode;
   int myargc = 2;
   char *myargv[2];
+#ifdef MULTI_THREAD
+   static th_context *th ;
+#endif
+
+#ifdef MULTI_THREAD
+  th = malloc( sizeof( th_context ) ) ;  /* don't use mem_alloc */
+#endif
 
   /* xsb_init relies on the calling program to pass the absolute or relative
      path name of the XSB installation directory. We assume that this directory
@@ -22,36 +32,36 @@ int main(int argc, char *argv[])
   myargv[1] = "-n";
 
   /* Initialize xsb */
-  xsb_init(myargc,myargv);  /* depend on user to put in right options (-n) */
+  xsb_init(CTXTc myargc,myargv);  /* depend on user to put in right options (-n) */
 
   /* Create command to consult a prolog file in argv[2] and send it. */
-  c2p_functor("consult",1,reg_term(1));
-  c2p_string(argv[2],p2p_arg(reg_term(1),1));
-  if (xsb_command()) printf("Error consulting %s.P.\n", argv[2]);
+  c2p_functor(CTXTc "consult",1,reg_term(CTXTc 1));
+  c2p_string(CTXTc argv[2],p2p_arg(reg_term(CTXTc 1),1));
+  if (xsb_command(CTXT)) printf("Error consulting %s.P.\n", argv[2]);
 
-  if (xsb_command_string("consult(basics).")) 
+  if (xsb_command_string(CTXTc "consult(basics).")) 
     printf("Error (string) consulting basics.\n");
 
   /* Create the query p(300,X,Y) and send it. */
-  c2p_functor("p",3,reg_term(1));
-  c2p_int(300,p2p_arg(reg_term(1),1));
+  c2p_functor(CTXTc "p",3,reg_term(CTXTc 1));
+  c2p_int(CTXTc 300,p2p_arg(reg_term(CTXTc 1),1));
 
-  rcode = xsb_query();
+  rcode = xsb_query(CTXT);
 
   /* Print out answer and retrieve next one. */
   while (!rcode) {
-    if (!(is_string(p2p_arg(reg_term(2),1)) & 
-	  is_string(p2p_arg(reg_term(2),2))))
+    if (!(is_string(p2p_arg(reg_term(CTXTc 2),1)) & 
+	  is_string(p2p_arg(reg_term(CTXTc 2),2))))
       printf("2nd and 3rd subfields must be atoms\n");
     else
       printf("Answer: %d, %s(%s), %s(%s)\n",
-	     p2c_int(p2p_arg(reg_term(1),1)),
-	     p2c_string(p2p_arg(reg_term(1),2)),
+	     p2c_int(p2p_arg(reg_term(CTXTc 1),1)),
+	     p2c_string(p2p_arg(reg_term(CTXTc 1),2)),
 	     xsb_var_string(1),
-	     p2c_string(p2p_arg(reg_term(1),3)),
+	     p2c_string(p2p_arg(reg_term(CTXTc 1),3)),
 	     xsb_var_string(2)
 	     );
-    rcode = xsb_next();
+    rcode = xsb_next(CTXT);
   }
 
   /* Create the string query p(300,X,Y) and send it, use higher-level
@@ -59,12 +69,12 @@ int main(int argc, char *argv[])
 
   xsb_make_vars(3);
   xsb_set_var_int(300,1);
-  rcode = xsb_query_string("p(X,Y,Z).");
+  rcode = xsb_query_string(CTXTc "p(X,Y,Z).");
 
   /* Print out answer and retrieve next one. */
   while (!rcode) {
-    if (!(is_string(p2p_arg(reg_term(2),2)) & 
-	  is_string(p2p_arg(reg_term(2),3))))
+    if (!(is_string(p2p_arg(reg_term(CTXTc 2),2)) & 
+	  is_string(p2p_arg(reg_term(CTXTc 2),3))))
        printf("2nd and 3rd subfields must be atoms\n");
     else
       printf("Answer: %d, %s, %s\n",
@@ -72,13 +82,13 @@ int main(int argc, char *argv[])
 	     xsb_var_string(2),
 	     xsb_var_string(3)
 	     );
-    rcode = xsb_next();
+    rcode = xsb_next(CTXT);
   }
 
 
 
   /* Close connection */
-  xsb_close();
+  xsb_close(CTXT);
   printf("cmain exit\n");
   return(0);
 }
