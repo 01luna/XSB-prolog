e6de93f4fe8705770bae6bf9f23f33f504edbd2f
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2007-06-27 00:50:20 +0000

    Some tests for handling maximum threads, and detached threads, along with more ISO tests.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@5352 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- mttests/prolog_tests/test.sh
+++ mttests/prolog_tests/test.sh
@@ -31,3 +31,4 @@ options=$3
 ../seqgentest.sh "$XEMU $options" cregtest_thread_make "test." 
 ../seqgentest.sh "$XEMU $options" cvartest_thread_make2 "test." 
 ../seqgentest.sh "$XEMU $options" cregtest_thread_make2 "test." 
+../seqgentest.sh "$XEMU --max_threads 100" detach_test "test." 
