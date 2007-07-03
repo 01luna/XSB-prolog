07939fcae3f62de197df2815664346d061f4201a
dwarren <dwarren@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2007-07-03 14:56:53 +0000

    Commented out a couple of debugging log messages.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@5356 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/memory_xsb.c
+++ XSB/emu/memory_xsb.c
@@ -19,7 +19,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: memory_xsb.c,v 1.46 2007-06-10 18:43:58 tswift Exp $
+** $Id: memory_xsb.c,v 1.47 2007-07-03 14:56:53 dwarren Exp $
 ** 
 */
 
@@ -306,7 +306,7 @@ Please use -c N or cpsize(N) to start with a larger choice point stack"
   cps_top = (byte *)top_of_cpstack;
   trail_top = (byte *)top_of_trail;
 
-  fprintf(stddbg,"Reallocating the Trail and Choice Point Stack data area");
+  //  fprintf(stddbg,"Reallocating the Trail and Choice Point Stack data area\n");
 
   /* Expand the Trail / Choice Point Stack Area
      ------------------------------------------ */
