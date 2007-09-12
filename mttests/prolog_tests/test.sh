679ee27bf0832a696a9219b42268221c2ad076ab
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2007-09-12 21:47:11 +0000

    Added tests (modified from what Rui sent) for destroying message queues and for thread_cancellation.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@5549 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- mttests/prolog_tests/test.sh
+++ mttests/prolog_tests/test.sh
@@ -35,3 +35,4 @@ options=$3
 ../seqgentest.sh "$XEMU $options" cregtest_thread_make2 "test." 
 ../seqgentest.sh "$XEMU --max_threads 100" detach_test "test." 
 ../seqgentest.sh "$XEMU $options" thread_property "test." 
+../seqgentest.sh "$XEMU $options" test_destroy "test." 
