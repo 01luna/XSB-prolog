9ff352769ee7d20dc814503490cae96cf6bf4c87
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2007-09-26 21:39:35 +0000

    Changes to test out private_foreign_pred as well as foreign_pred in higher level C interface.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@5565 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/prolog_tests/second_foreign.c
+++ xsbtests/prolog_tests/second_foreign.c
@@ -1,3 +1,5 @@
+#include "context.h"
+
 int add_integers(int a, int b)
 {
   return a+b;
@@ -16,6 +18,41 @@ void swap_ints(int *a, int *b)
 }
 
 void sort(char *A)
+{
+  int i,flag = 1;
+  char temp;
+  /* bubble-sort */
+  
+  while (flag) {
+    flag = 0;
+    for (i=0; A[i+1]!='\0'; i++) 
+      if (A[i] > A[i+1]) {
+	temp = A[i];
+	A[i] = A[i+1];
+	A[i+1] = temp;
+	flag = 1;
+      }
+  }
+}
+
+int context_add_integers(CTXTdeclc int a, int b)
+{
+  return a+b;
+}
+
+double context_add_floats(CTXTdeclc double a, double b)
+{
+  return a+b;
+}
+
+void context_swap_ints(CTXTdeclc int *a, int *b)
+{
+  int c = *a;
+  *a = *b;
+  *b = c;
+}
+
+void context_sort(CTXTdeclc char *A)
 {
   int i,flag = 1;
   char temp;
