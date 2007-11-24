e047562d76b7f963bbbfff72d43750bb43b6aa5e
ruim <ruim@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2007-11-24 07:38:40 +0000

    rolled back last commit it was unsafe
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@5672 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/bineg_xsb_i.h
+++ XSB/emu/bineg_xsb_i.h
@@ -18,7 +18,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: bineg_xsb_i.h,v 1.35 2007-11-24 06:53:58 ruim Exp $
+** $Id: bineg_xsb_i.h,v 1.36 2007-11-24 07:38:40 ruim Exp $
 ** 
 */
 
@@ -136,14 +136,11 @@ case IS_INCOMPLETE: {
         waiting_for_thread = find_context(table_tid) ;
         if( would_deadlock( table_tid, xsb_thread_id ) )
 	{	/* code for leader */
-     		pthread_mutex_unlock(&completing_mut);
                 reset_other_threads( th, waiting_for_thread, producerSF );
-     		pthread_mutex_lock(&completing_mut);
-                reset_thread_deps( th, waiting_for_thread, producerSF );
                 th->deadlock_brk_leader = TRUE ;
                 pthread_cond_broadcast(&completing_cond) ;
-                pthread_mutex_unlock(&completing_mut) ;
 		reset_leader( th ) ;
+                pthread_mutex_unlock(&completing_mut) ;
 		return TRUE ;
 	}
 	th->waiting_for_subgoal = producerSF ;
