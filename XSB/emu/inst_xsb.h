28f6b2dbd1a3d7a02ef92f983366641473a1b9a2
dwarren <dwarren@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2008-02-04 03:59:53 +0000

    Add a new xwam instruction,cmpreg, replaces subreg for comparisons to avoid overflow problems.  Now minint is less than maxint....
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@5820 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/inst_xsb.h
+++ XSB/emu/inst_xsb.h
@@ -19,7 +19,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: inst_xsb.h,v 1.30 2008-01-30 16:03:03 dwarren Exp $
+** $Id: inst_xsb.h,v 1.31 2008-02-04 03:59:53 dwarren Exp $
 ** 
 */
 
@@ -216,6 +216,7 @@ extern Cell inst_table[BUILTIN_TBL_SZ][5];
 #define bldtval_putlist_bldnumcon 0x24  /* combined, same reg, 16-bit int */
 #define bldtvar_list_numcon 0x25  /* combined, same reg, 16-bit int */
 #define getkpvars	0x26 /* get k pvars, k>=2 */
+#define cmpreg		0x2c /* numeric compare op1 to op2 and put -1,0,1 in op2, op2 cmp op1 */
 #define addintfastuni	0x2d /* add int to t/p var and unify with t/p var */
 #define addintfastasgn	0x2e /* add int to t/p var and assign to t/p var */
 #define xorreg          0x2f
