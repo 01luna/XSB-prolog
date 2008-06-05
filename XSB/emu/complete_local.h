c78d6d5acf251268e7e732362609674f32ab4531
ruim <ruim@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2008-06-05 23:28:18 +0000

    improvements for shared completed tables: 	decreased lock time on completion 	avoided waking up threads for private tables
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@6012 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/emu/complete_local.h
+++ XSB/emu/complete_local.h
@@ -18,7 +18,7 @@
 ** along with XSB; if not, write to the Free Software Foundation,
 ** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: complete_local.h,v 1.15 2008-06-05 18:15:11 ruim Exp $
+** $Id: complete_local.h,v 1.16 2008-06-05 23:28:18 ruim Exp $
 ** 
 */
 #ifndef __COMPLETE_LOCAL_H__
@@ -315,21 +315,23 @@ static inline void CompleteSimplifyAndReclaim(CTXTdeclc CPtr cs_ptr)
       
   /* reclaim all answer lists, but the one for the leader */
   ComplStkFrame = next_compl_frame(cs_ptr);
-#ifdef SHARED_COMPL_TABLES
-  pthread_mutex_lock( &completing_mut );
-#endif
   while (ComplStkFrame >= openreg) {
     compl_subg = compl_subgoal_ptr(ComplStkFrame);
-#ifdef SHARED_COMPL_TABLES
-    pthread_cond_broadcast( &TIF_ComplCond(subg_tif_ptr(compl_subg)) );
-#endif
     reclaim_incomplete_table_structs(compl_subg);
     ComplStkFrame = next_compl_frame(ComplStkFrame);
   } /* while */
+
 #ifdef SHARED_COMPL_TABLES
-  /* wake up the threads waiting for the leader to! */
-  compl_subg = compl_subgoal_ptr(ComplStkFrame);
-  pthread_cond_broadcast( &TIF_ComplCond(subg_tif_ptr(compl_subg)) );
+  /* now wake up all threads suspended on tables just completed
+     including leader */
+  pthread_mutex_lock( &completing_mut );
+  ComplStkFrame = cs_ptr;
+  while (ComplStkFrame >= openreg) {
+    compl_subg = compl_subgoal_ptr(ComplStkFrame);
+    if ( IsSharedSF(compl_subg) )
+       pthread_cond_broadcast( &TIF_ComplCond(subg_tif_ptr(compl_subg)) );
+    ComplStkFrame = next_compl_frame(ComplStkFrame);
+  } /* while */
   pthread_mutex_unlock( &completing_mut );
 #endif
 }
