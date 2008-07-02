368817fabb2a3d24a9f9d44fc864d75707a02018
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2008-07-02 21:29:35 +0000

    Working on shared omega benchmarks.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@6027 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- mttests/benches/petri/benchomega.sh
+++ mttests/benches/petri/benchomega.sh
@@ -1,6 +1,7 @@
 XEMU=$1
 MAX=$2
 N=$3
+# TLS currently testing with N = 8000
 
 FILE=./omegaRes.P
 
@@ -12,8 +13,8 @@ do
 
 echo "-------------------- Testing $nthreads threads ------------------------------"
 
-	../genbench.sh "$XEMU" "[benchmark_omega], gen_private_omega($N)." \
-			"private_scale_omega($nthreads)." "$FILE"
+	../genbench.sh "$XEMU" "[benchmark_omega]." \
+			"shared_bench_omega(400000,$nthreads)." "$FILE"
 
 	nthreads=$[$nthreads*2]
 done
