79948d7a2054434f02c5d7a7d8e7b7c054b1c066
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2008-07-03 22:14:53 +0000

    now trying shared prefs.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@6039 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- mttests/benches/petri/benchprefs.sh
+++ mttests/benches/petri/benchprefs.sh
@@ -1,7 +1,7 @@
 XEMU=$1
 MAX=$2
 
-FILE=./constrRes.P
+FILE=./prefsRes.P
 
 rm -f $FILE
 
@@ -14,6 +14,8 @@ echo "-------------------- Testing $nthreads threads ---------------------------
 	../genbench.sh "$XEMU" "[benchmark_prefs]." \
 			"private_scale_prefs(199,$nthreads,$MAX)." "$FILE"
 
+	../genbench.sh "$XEMU" "[benchmark_prefs]." \
+			"shared_bench_prefs(199,$nthreads,$MAX)." "$FILE"
 
 	nthreads=$[$nthreads*2]
 done
