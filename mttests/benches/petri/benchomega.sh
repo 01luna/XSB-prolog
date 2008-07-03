0e57c648a27a87591f2b146f2a9aec13048a0be8
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2008-07-03 22:40:26 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@6043 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- mttests/benches/petri/benchomega.sh
+++ mttests/benches/petri/benchomega.sh
@@ -1,7 +1,8 @@
 XEMU=$1
 MAX=$2
 N=$3
-# TLS currently testing with N = 8000
+
+# TLS currently testing with N = 8000 for private
 
 FILE=./omegaRes.P
 
@@ -13,11 +14,11 @@ do
 
 echo "-------------------- Testing $nthreads threads ------------------------------"
 
-	../genbench.sh "$XEMU" "[benchmark_omega], gen_private_omega($N)." \
+	../genbench.sh "$XEMU" "[benchmark_omega], gen_private_omega(7000)." \
 			"private_scale_omega($nthreads)." "$FILE"
 
 	../genbench.sh "$XEMU" "[benchmark_omega]." \
-			"shared_bench_omega(400000,$MAX,$nthreads)." "$FILE"
+			"shared_bench_omega(300000,$MAX,$nthreads)." "$FILE"
 
 	nthreads=$[$nthreads*2]
 done
