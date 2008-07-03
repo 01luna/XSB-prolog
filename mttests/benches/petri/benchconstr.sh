f856010ade86e5b675e86a0a4a8829f49386f581
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2008-07-03 20:13:27 +0000

    A bit more analysis on elemnet and set up contraint nets for private thread testing.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@6031 756869e0-9e47-4bc5-b0a8-847305dc5e1c

---
+++ mttests/benches/petri/benchconstr.sh
@@ -0,0 +1,19 @@
+XEMU=$1
+MAX=$2
+
+FILE=./omegaRes.P
+
+rm -f $FILE
+
+nthreads=1
+while [ $nthreads -le $MAX ]
+do
+
+echo "-------------------- Testing $nthreads threads ------------------------------"
+
+	../genbench.sh "$XEMU" "[benchmark_constraint]." \
+			"private_scale_constr(501,$nthreads,1)." "$FILE"
+
+	nthreads=$[$nthreads*2]
+done
+
