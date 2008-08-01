a37024d8ef4aff1e45802da38e8cd4c455ac9c31
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2008-08-01 19:10:36 +0000

    Adding Petri benchmark for call subsumption.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@6065 756869e0-9e47-4bc5-b0a8-847305dc5e1c

---
+++ mttests/benches/petri/benchelemcs.sh
@@ -0,0 +1,18 @@
+PREF=$1
+MAX=$2
+
+XEMU=$PREF/bin/xsb-mt
+FILE=./elemRes.P
+
+rm -f $FILE
+
+nthreads=1
+while [ $nthreads -le $MAX ]
+do
+echo "-------------------- Testing $nthreads threads ------------------------------"
+    ../genbench.sh "$XEMU" "[benchmark_elem], gen_proc_2(500,$MAX)." \
+"bench_private_process_cs($nthreads)." "$FILE"
+
+nthreads=$[$nthreads*2]
+done
+
