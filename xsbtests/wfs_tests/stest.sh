f9e34da990e628dc4d850c5f7db9ce870bc5b0b5
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2009-01-17 19:47:42 +0000

    Trying again...
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@6236 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/wfs_tests/stest.sh
+++ xsbtests/wfs_tests/stest.sh
@@ -13,5 +13,5 @@ file_list=p*.P
 for file in $file_list ; do
 	prog=`basename $file .P`
 	# XEMU and options must be together in quotes
-	./sgentest.sh "$XEMU $options" wfs_test $prog "test($prog)."
+	sh ./sgentest.sh "$XEMU $options" wfs_test $prog "test($prog)."
 done
