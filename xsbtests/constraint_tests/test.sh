e688aabae0923f8923573b72ca9b5228015a4a8c
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2009-10-22 16:14:00 +0000

    Adding tests for constraints in call subsumption.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@6425 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/constraint_tests/test.sh
+++ xsbtests/constraint_tests/test.sh
@@ -19,6 +19,7 @@ opts=$2
 ../gentest.sh "$XEMU $opts" clprtest "test."
 ../gentest.sh "$XEMU $opts" test_bounds "test."
 ../gentest.sh "$XEMU $opts" residualAttv "test."
+../gentest.sh "$XEMU $opts" callsubs_constraints "test."
 
 # test of C-calling XSB w. constraints.
 ../gentest.sh "$XEMU $opts" cvarconstr_make "test."
