594124aae0c7d6bbd81fb9a8599a4dcd261d28ca
rzf <rzf@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2010-05-11 10:04:29 +0000

    adaptation of pita for installation
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@6613 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/packages/pita/pita_lib.c
+++ XSB/packages/pita/pita_lib.c
@@ -1,17 +1,34 @@
-/* 
-  PITA
-  
-Copyright (c) 2010, Fabrizio Riguzzi and Terrance Swift
+/* File:      pita_lib.c
+** Author(s): Fabrizio Riguzzi and Terrance Swift
+** Contact:   fabrizio.riguzzi@unife.it, xsb-contact@cs.sunysb.edu
+**
+** Copyright (C) Copyright: Fabrizio Riguzzi and Terrance Swift
+**                          ENDIF - University of Ferrara
+**                          Centro de Inteligencia Artificial,
+**                          Universidade Nova de Lisboa, Portugal
+**
+** XSB is free software; you can redistribute it and/or modify it under the
+** terms of the GNU Library General Public License as published by the Free
+** Software Foundation; either version 2 of the License, or (at your option)
+** any later version.
+**
+** XSB is distributed in the hope that it will be useful, but WITHOUT ANY
+** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+** FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for
+** more details.
+**
+** You should have received a copy of the GNU Library General Public License
+** along with XSB; if not, write to the Free Software Foundation,
+** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+**
+*/
 
+/* 
 This package uses the library cudd, see http://vlsi.colorado.edu/~fabio/CUDD/
 for the relative license.
-
 */
 #include "util.h"
 #include "cudd.h"
-//#include "array.h"
-//#include "mtr.h"
-//#include "avl.h"
 #include <glib.h>
 #include <math.h>
 #include <stdlib.h>
