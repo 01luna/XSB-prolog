81c870b3ca2f8d34ad0643c4889740e5ea5be46b
kifer <kifer@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2011-04-29 01:07:00 +0000

    added for better win32/64 config
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@6953 756869e0-9e47-4bc5-b0a8-847305dc5e1c

---
+++ XSB/build/makexsb64.bat
@@ -0,0 +1,51 @@
+@echo off
+REM   makexsb.bat
+REM   Script for compiling XSB under Windows using VC++
+
+set XSBCONFIGdir=..\config\x86-pc-windows64
+
+IF NOT EXIST %XSBCONFIGdir%\saved.o MKDIR %XSBCONFIGdir%\saved.o
+IF NOT EXIST %XSBCONFIGdir%\bin mkdir %XSBCONFIGdir%\bin
+IF NOT EXIST %XSBCONFIGdir%\lib mkdir %XSBCONFIGdir%\lib
+
+@copy odbc\* %XSBCONFIGdir%
+@copy windows64\banner.msg %XSBCONFIGdir%
+@copy windows64\xsb_configuration.P %XSBCONFIGdir%\lib
+@copy windows64\xsb_config.h %XSBCONFIGdir%
+@copy windows64\xsb_config_aux.h %XSBCONFIGdir%
+@copy windows64\xsb_debug.h %XSBCONFIGdir%
+
+@cd ..\emu
+
+REM Concatenate MSVC_mkfile.mak & MSVC.dep into emu\MSVC_mkfile.mak
+@copy ..\build\windows64\MSVC_mkfile.mak+..\build\MSVC.dep MSVC_mkfile.mak
+
+
+@nmake /nologo /f "MSVC_mkfile.mak" %1 %2 %3 %4 %5 %6 %7
+
+@del MSVC_mkfile.mak
+
+@cd ..\gpp
+@nmake /nologo /s /f "MSVC_mkfile64.mak" %1 %2 %3 %4 %5 %6 %7
+
+@cd ..\packages
+
+@cd dbdrivers
+@nmake /nologo /s /f NMakefile64.mak
+@cd ..
+
+REM build curl before sgml and xpath
+@cd curl
+@nmake /nologo /f NMakefile64.mak
+@cd ..
+
+@cd sgml\cc
+@nmake /nologo /f NMakefile64.mak
+@cd ..\..
+
+@cd xpath\cc
+@nmake /nologo /f NMakefile64.mak
+@cd ..\..
+
+@cd ..\build
+
