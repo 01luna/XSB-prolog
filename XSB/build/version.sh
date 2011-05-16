86e1b58b6489706e7976f90521d8db2dd8510f49
kifer <kifer@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2011-05-16 01:04:24 +0000

    code to remove build/extensions_xsb.in and add emu/extensions_xsb.h instead. Otherwise: windows build probs.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@7015 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/build/version.sh
+++ XSB/build/version.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Do not touch xsb_patch_date! It is updated by CVS.
-xsb_patch_date='$Date: 2011-05-15 07:18:34 $'
+xsb_patch_date='$Date: 2011-05-16 01:04:24 $'
 
 xsb_build_date=`date`
 xsb_patch_date=`echo $xsb_patch_date | sed 's,.*D,Patch d,' | sed 's, *\\$,,'`
@@ -28,6 +28,5 @@ xsb_codename="Pignoletto"    # Version 3.3 Italian Beer
 #xsb_release_date=2009-03-15
 #xsb_release_date=2011-04-12
 xsb_release_date=2011-05-11
-
 
 
