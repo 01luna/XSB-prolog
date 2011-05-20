6ec27fd8f4abb23a65125d3f4b95634e1585e4c4
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2011-05-20 15:50:13 +0000

    I think this will fix the version?
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@7035 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/build/windows64/xsb_config.h
+++ XSB/build/windows64/xsb_config.h
@@ -48,8 +48,8 @@
 
 
 #define WIN_NT 1
-#define RELEASE_DATE 2011-05-11
-#define XSB_VERSION "3.3.2 (Pignoletto)"
+#define RELEASE_DATE 2011-05-20
+#define XSB_VERSION "3.3.3 (Pignoletto)"
 
 /* this is used by many to check if config.h was included in proper order */
 #ifndef CONFIG_INCLUDED
@@ -60,7 +60,7 @@
 /* #undef FAST_FLOATS */
 
 /* Use local eval strategy. This is the default. */
-/* #undef LOCAL_EVAL */
+#define LOCAL_EVAL 1
 
 /* Define, if XSB is built with support for ORACLE DB */
 /* #undef ORACLE */
@@ -103,4 +103,4 @@
 /* GC on SLG-WAM! ;) */
 #define SLG_GC 1
 
-/* #undef NON_OPT_COMPILE */
+#define NON_OPT_COMPILE 1
