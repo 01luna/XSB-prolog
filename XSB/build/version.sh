4a04bbead70f937abd2eae03f9123d9af0c60c43
dwarren <dwarren@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2011-06-27 15:58:08 +0000

    New commits.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@7106 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/build/version.sh
+++ XSB/build/version.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Do not touch xsb_patch_date! It is updated by CVS.
-xsb_patch_date='$Date: 2011-06-18 04:30:37 $'
+xsb_patch_date='$Date: 2011-06-27 15:58:08 $'
 
 xsb_build_date=`date`
 xsb_patch_date=`echo $xsb_patch_date | sed 's,.*D,Patch d,' | sed 's, *\\$,,'`
@@ -29,4 +29,3 @@ xsb_codename="Pignoletto"    # Version 3.3 Italian Beer
 #xsb_release_date=2011-04-12
 #xsb_release_date=2011-05-11
 xsb_release_date=2011-05-21
-
