6ed3ba1c270fc1ca1b99bceefb50974e23e3f019
kifer <kifer@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2011-07-08 04:32:08 +0000

    fixed make depend. Was not working correctly since v 1.57.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@7117 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/build/version.sh
+++ XSB/build/version.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Do not touch xsb_patch_date! It is updated by CVS.
-xsb_patch_date='$Date: 2011-07-02 14:55:40 $'
+xsb_patch_date='$Date: 2011-07-08 04:32:08 $'
 
 xsb_build_date=`date`
 xsb_patch_date=`echo $xsb_patch_date | sed 's,.*D,Patch d,' | sed 's, *\\$,,'`
@@ -29,3 +29,4 @@ xsb_codename="Pignoletto"    # Version 3.3 Italian Beer
 #xsb_release_date=2011-04-12
 #xsb_release_date=2011-05-11
 xsb_release_date=2011-07-02
+
