73512174aece325f9e60ff6253afaf4e291a0c04
kifer <kifer@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2011-08-06 05:53:11 +0000

    patch date change
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@7187 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/build/version.sh
+++ XSB/build/version.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Do not touch xsb_patch_date! It is updated by CVS.
-xsb_patch_date='$Date: 2011-08-03 04:37:58 $'
+xsb_patch_date='$Date: 2011-08-06 05:51:51 $'
 
 xsb_build_date=`date`
 xsb_patch_date=`echo $xsb_patch_date | sed 's,.*D,Patch d,' | sed 's, *\\$,,'`
@@ -29,5 +29,6 @@ xsb_codename="Pignoletto"    # Version 3.3 Italian Beer
 #xsb_release_date=2011-04-12
 #xsb_release_date=2011-05-11
 xsb_release_date=2011-07-02
+
 
 
