ce1f6dbbad0ae42ad0b6021d82135574cb199f9f
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2012-08-12 14:24:31 +0000

    Trying to get new date for version.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@7596 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/build/version.sh
+++ XSB/build/version.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
 # Do not touch xsb_patch_date! It is updated by CVS.
-xsb_patch_date='$Date: 2012-03-09 18:27:01 $'
+xsb_patch_date='$Date: 2012-08-12 14:24:31 $'
 
 xsb_build_date=`date`
 xsb_patch_date=`echo $xsb_patch_date | sed 's,.*D,Patch d,' | sed 's, *\\$,,'`
 
 xsb_major_version=3
 xsb_minor_version=3
-xsb_patch_version=6
+xsb_patch_version=7
 #xsb_beta_version=pre"
 #xsb_codename="Skol"        # for Version 2.1
 #xsb_codename="Tsingtao"    # for Version 2.2
@@ -29,7 +29,7 @@ xsb_codename="Pignoletto"    # Version 3.3 Italian Beer
 #xsb_release_date=2011-04-12
 #xsb_release_date=2011-05-11
 #xsb_release_date=2011-07-02
-xsb_release_date=2012-01-02
+#xsb_release_date=2012-08-25
 
 
 
