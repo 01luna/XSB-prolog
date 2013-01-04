44d82f9dcd8c053bfdffe655e9379d400b80a527
dwarren <dwarren@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2013-01-04 13:55:12 +0000

    Update to make more files.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@7691 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/prolog_tests/make_wind_dlls64.bat
+++ xsbtests/prolog_tests/make_wind_dlls64.bat
@@ -4,3 +4,6 @@ REM   foreign calls in this directory under --mswin64 testing.
 nmake /f MakefileCinter1_w64
 nmake /f MakefileCinter2_w64
 nmake /f MakefileCinter3_w64
+nmake /f MakefileFibr_w64
+nmake /f MakefileCCallsXSB_w64
+nmake /f MakefileXeddis_w64
