622b26caeddb79e701b18cdfcdf60e03bca869fa
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2013-02-15 20:53:58 +0000

    Testing assert w. register allocation & attributed variables.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@7725 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/attv_tests/test.sh
+++ xsbtests/attv_tests/test.sh
@@ -22,6 +22,7 @@ opts=$2
 ../gentest.sh "$XEMU $opts" ret_attv "test."
 ../gentest.sh "$XEMU $opts" fd1 "test."
 ../gentest.sh "$XEMU $opts" general "test."
+../gentest.sh "$XEMU $opts" attvar_assert_regalloc "test."
 
 # the following is obsolete (attvs now allowed in subsumptive tables)
 #../gentest.sh "$XEMU $opts" catch1 "test."
