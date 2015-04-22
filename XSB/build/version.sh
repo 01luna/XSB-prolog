bcbe4da3b33f927b460f3438628942efab62c633
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2015-04-22 18:05:20 +0000

    Committing for new version
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@8332 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/build/version.sh
+++ XSB/build/version.sh
@@ -15,8 +15,8 @@ xsb_patch_date=`echo Build date: $xsb_build_date`
 
 
 xsb_major_version=3
-xsb_minor_version=5
-xsb_patch_version=1
+xsb_minor_version=6
+#xsb_patch_version=1
 #xsb_beta_version=pre"
 #xsb_codename="Skol"        # for Version 2.1
 #xsb_codename="Tsingtao"    # for Version 2.2
@@ -30,7 +30,8 @@ xsb_patch_version=1
 #xsb_codename="Kopi Lewak"  # Version 3.2 Civit coffee
 #xsb_codename="Pignoletto"  # Version 3.3 Italian Beer (Fabrizio)
 #xsb_codename="Soy mILK"    # Version 3.4 (Benjamin)
-xsb_codename="Maotai"       # Version 3.5 Chinese Beer (Neng-Fa)
+#xsb_codename="Maotai"      # Version 3.5 Chinese Beer (Neng-Fa)
+xsb_codename="Gazpatcho"    # Version 3.6 (Paulo Moura)
 
 # Format: YYYY-MM-DD or YYYY.MM.DD or YYYY/MM/DD
 # With this, XSB should become Y2K compliant :-)
@@ -40,5 +41,6 @@ xsb_codename="Maotai"       # Version 3.5 Chinese Beer (Neng-Fa)
 #xsb_release_date=2011-05-11
 #xsb_release_date=2011-07-02
 #xsb_release_date=2013-05-01
-xsb_release_date=2014-07-06
+#xsb_release_date=2014-07-06
+xsb_release_date=2015-04-28 # target (may be changed)
 
