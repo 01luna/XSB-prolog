5989a578b5947ad5f1150508f147b425dad4a3d8
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2016-01-01 23:40:26 +0000

    Initial test for tripwires.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@8558 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/table_tests/test.sh
+++ xsbtests/table_tests/test.sh
@@ -165,6 +165,8 @@ rm test_ec.xwam
 ../gentest.sh "$XEMU -l $options" test_trans_bug "test."
 #--------------------------------------------------
 ../gentest.sh "$XEMU -l $options" test_sdg_preds "test."
+#--------------------------------------------------
+../gentest.sh "$XEMU -l $options" test_trip "test."
 
 #VALGRIND
 if test "$valgrind" = "true"; then
