c1f55ddc30d54e9ff91bb547c6a57de6c5728e7d
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2016-09-26 14:35:39 +0000

    Forgot to delete a test that's been moved out of nonmt (and so is mt safe now).
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@8821 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/nonmt_tests/test.sh
+++ xsbtests/nonmt_tests/test.sh
@@ -28,8 +28,6 @@ options=$2
 #-------------------------------------------------- yet more tests of invalidation
 ../gentest.sh "$XEMU -l $options" test_invalidate "test."
 #--------------------------------------------------
-../gentest.sh "$XEMU -l $options" test_table_errors "test."
-#--------------------------------------------------
 ../gentest.sh "$XEMU -l $options" test_goal_interrupt "test."
 #--------------------------------------------------
 ../gentest.sh "$XEMU -l $options" test_trip "test."
