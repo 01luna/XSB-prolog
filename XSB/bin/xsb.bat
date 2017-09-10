a7cf54dcc9602ad29b379f071dfddb288211a5b6
"kifer" <"kifer"@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2017-09-10 18:28:30 +0000

    changed %0\.. to %~dp0, which is supposedly more robust.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@9066 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/bin/xsb.bat
+++ XSB/bin/xsb.bat
@@ -1,6 +1,7 @@
 @echo off
 REM Script for running XSB when built natively on Windows x86
 
-set BINDIR=%0\..\..
+REM set XSBDIR=%0\..\..
+set XSBDIR=%~dp0\..
 
-%BINDIR%\config\x86-pc-windows\bin\xsb %1 %2 %3 %4 %5 %6 %7
+%XSBDIR%\config\x86-pc-windows\bin\xsb %1 %2 %3 %4 %5 %6 %7
