805ad5d7469721564064f62ce4e518f8498a7763
"kifer" <"kifer"@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2017-09-16 06:41:27 +0000

    in xsb*.bat, added quotes for the cases when the path name has spaces
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@9073 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/bin/xsb.bat
+++ XSB/bin/xsb.bat
@@ -4,4 +4,4 @@ REM Script for running XSB when built natively on Windows x86
 REM set XSBDIR=%0\..\..
 set XSBDIR=%~dp0\..
 
-%XSBDIR%\config\x86-pc-windows\bin\xsb %1 %2 %3 %4 %5 %6 %7
+"%XSBDIR%"\config\x86-pc-windows\bin\xsb %1 %2 %3 %4 %5 %6 %7
