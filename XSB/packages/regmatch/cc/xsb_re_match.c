323856bc1c504a6662fc237a5a07682b4e20cd78
"kifer" <"kifer"@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2019-02-19 01:58:43 +0000

    changed license to apache for the packages I was involved in
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@9548 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- XSB/packages/regmatch/cc/xsb_re_match.c
+++ XSB/packages/regmatch/cc/xsb_re_match.c
@@ -4,21 +4,17 @@
 ** 
 ** Copyright (C) The Research Foundation of SUNY, 1999
 ** 
-** XSB is free software; you can redistribute it and/or modify it under the
-** terms of the GNU Library General Public License as published by the Free
-** Software Foundation; either version 2 of the License, or (at your option)
-** any later version.
-** 
-** XSB is distributed in the hope that it will be useful, but WITHOUT ANY
-** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-** FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for
-** more details.
-** 
-** You should have received a copy of the GNU Library General Public License
-** along with XSB; if not, write to the Free Software Foundation,
-** Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
 **
-** $Id: xsb_re_match.c,v 1.27 2010-08-19 15:03:39 spyrosh Exp $
+**      http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
 ** 
 */
 
