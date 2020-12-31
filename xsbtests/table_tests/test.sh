f8124332cf4e56a0c75575368b9fb1601bd20079
tswift <tswift@756869e0-9e47-4bc5-b0a8-847305dc5e1c>
2020-12-31 23:18:33 +0000

    Test for not_exists.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xsb/src/trunk@9769 756869e0-9e47-4bc5-b0a8-847305dc5e1c

--- xsbtests/table_tests/test.sh
+++ xsbtests/table_tests/test.sh
@@ -207,6 +207,8 @@ rm -f aggregs_test_2.xwam
 ../gentest.sh "$XEMU -l $options" test_table_index "test."
 #-------------------------------------------------
 ../gentest.sh "$XEMU -l $options" test_floundering_cleanup "test."
+#-------------------------------------------------
+../gentest.sh "$XEMU -l $options" not_exists_test "test."
 #--------------------------------
 # h series is hilog/tabling tests
 #------------------------------------
