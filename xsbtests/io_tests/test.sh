81a61861aa1380a734f394b209e55b76cacf5cbc
Theresa Swift <tswift@bbn.com>
2021-03-11 13:07:58 -0500

    Adding a test of proc_files to make sure we don't break dsv file read/write.

--- xsbtests/io_tests/test.sh
+++ xsbtests/io_tests/test.sh
@@ -10,4 +10,5 @@ options=$2
 # XEMU and options must be together in quotes
 ../gentest.sh "$XEMU $options" io_basics "test."
 ../gentest.sh "$XEMU $options" fmt_io "test."
+../gentest.sh "$XEMU $options" test_proc_files "test."
 #../gentest.sh "$XEMU $options" io_timeout "test."
