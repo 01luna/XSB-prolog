f0293c025ee04fb23cc6161d37bc0475243ec05c
Theresa Swift <tswift@bbn.com>
2021-04-29 10:47:09 -0400

    Improvements to the xsbpy testscript.

--- XSB/packages/xsbpy/test/test.sh
+++ XSB/packages/xsbpy/test/test.sh
@@ -3,3 +3,13 @@
 
 grep '!!!' test_xsbpy_out.txt
 
+status=0
+diff -w  test_xsbpy_out.txt test_xsbpy_out_old || status=1
+if test "$status" = 0 ; then 
+	echo "xsbpy tested"
+	rm -f test_xsbpy_out.txt
+else
+	echo "test_xsbpy_outs differ!!!"
+	diff -w  test_xsbpy_out.txt test_xsbpy_out_old
+fi
+
