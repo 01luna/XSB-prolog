f65da03950034b1933c136bf9f601b8623883398
Theresa Swift <tswift@bbn.com>
2021-05-21 16:08:15 -0400

    Added a few more tests for pymeth/4, for returning attirbutes and handling errors.

--- XSB/packages/xsbpy/test/returnVal.py
+++ XSB/packages/xsbpy/test/returnVal.py
@@ -6,11 +6,23 @@ def returnSet():
         X = ['"foo"',"'bar'",{1,"hello",('a','b',7)}]
         return(X)
 
+def easy_returnSet():
+        X = {1,"hello",('a','b',7)}
+        return(X)
+
 def return_None():
         return(None)
 
 def return_True():
         return(True)
+
+def return_pi():
+        return(3.14159)
+
+def print_elt(Elt):
+        print(type(Elt))
+        print(str(Elt))
+        return(Elt)
 
 def return_False():
         return(False)
--- XSB/packages/xsbpy/test/test.sh
+++ XSB/packages/xsbpy/test/test.sh
@@ -6,10 +6,10 @@ grep '!!!' test_xsbpy_out.txt
 status=0
 diff -w  test_xsbpy_out.txt test_xsbpy_out_old || status=1
 if test "$status" = 0 ; then 
-	echo "xsbpy tested"
+	echo "xsbpy successfuly tested"
 	rm -f test_xsbpy_out.txt
 else
-	echo "test_xsbpy_outs differ!!!"
+	echo "xsbpy test failed: test_xsbpy_outs differ!!!"
 	diff -w  test_xsbpy_out.txt test_xsbpy_out_old
 fi
 
