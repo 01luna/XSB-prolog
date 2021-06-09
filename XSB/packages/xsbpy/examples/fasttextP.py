fad6cdd1888cbc6d4d6ba08df6b4197df40dd096
root <root@arl-maturin.ad.bbn.com>
2021-06-09 14:04:07 -0400

    New xsbpy-fasttext interface supporting language identification

---
+++ XSB/packages/xsbpy/examples/fasttextP.py
@@ -0,0 +1,26 @@
+import fasttext as ft
+
+def hello_world(binpath):
+    sentence = ['Bonjour le monde']
+    print(sentence[0])
+    model = load_model(binpath)
+    predictions = model.predict(sentence)
+    lang = predictions[0][0][0]
+    confidence = predictions[1][0][0]
+    return(lang,confidence)
+
+# Loads training model from the file 
+def load_model(binpath):  
+    print('Loading training model',binpath)
+    model = ft.load_model(binpath)
+    print(type(model))
+    return(model)
+
+# FastText.predict() as a python function
+def predict2(binpath, sentences):
+    model = ft.load_model(binpath)
+    print(type(model))
+    predictions = model.predict(sentences)
+    lang = predictions[0][0][0]
+    confidence = predictions[1][0][0]
+    return((lang,confidence))
