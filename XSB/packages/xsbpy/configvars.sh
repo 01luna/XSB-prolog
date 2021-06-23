58b3725d92c8d39215b78d2df0133a2abbf3246e
Michael Kifer <kifer@cs.stonybrook.edu>
2021-06-23 18:32:38 -0400

    added settings for XSB libdir and infrastructure for transferring needed settings from the main configure script to xsbpy config

---
+++ XSB/packages/xsbpy/configvars.sh
@@ -0,0 +1,3 @@
+
+%%  /home/kifer/XSB/XSB-git/XSB/packages/xsbpy/configvars.sh.  Generated from configvars.in by configure.
+XSBCONFIG_LIBDIR="/home/kifer/XSB/XSB-git/XSB/config/x86_64-unknown-linux-gnu/lib"
