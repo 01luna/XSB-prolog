c76be753bc985643c88b5ecc110d5d0d1ed9e2f0
Theresa E. Swift (ADM) <tswift-adm@arl-maturin.ad.bbn.com>
2021-06-25 16:30:48 -0400

    Renaming xp_fasttext.py and including some changes.

--- XSB/packages/xsbpy/examples/xp_fasttext.py
+++ XSB/packages/xsbpy/examples/xp_fasttext.py
@@ -3,6 +3,12 @@
 
 import fasttext as ft
 
+def fasttext_predict(model,sentences): 
+    predictions = model.predict(sentences)
+    lang = predictions[0][0]
+    confidence = predictions[1][0]
+    return((lang,confidence))
+
 def hello_world(binpath):
     sentence = ['Bonjour le monde']
     print(sentence[0])
@@ -12,14 +18,7 @@ def hello_world(binpath):
     confidence = predictions[1][0][0]
     return(lang,confidence)
 
-# Loads training model from the file 
-def load_model(binpath):  
-    print('Loading training model',binpath)
-    model = ft.load_model(binpath)
-    print(type(model))
-    return(model)
-
-# FastText.predict() as a python function
+# Load and predict a the language of a string
 def predict2(binpath, sentences):
     model = ft.load_model(binpath)
     print(type(model))
@@ -27,3 +26,13 @@ def predict2(binpath, sentences):
     lang = predictions[0][0][0]
     confidence = predictions[1][0][0]
     return((lang,confidence))
+
+# Not needed -- just use fasttext load_model
+# Loads training model from the file
+#def load_model(binpath):  
+#    print('Loading training model',binpath)
+#    model = ft.load_model(binpath)
+#    print(type(model))
+    return(model)
+
+
