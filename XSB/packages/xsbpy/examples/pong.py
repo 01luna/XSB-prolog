7c05189dc10da2f5871937d7613f7ce46644cd3c
Theresa Swift <tswift@bbn.com>
2021-07-30 13:24:50 -0400

    Rearranging directory structure for xsbpy.

--- XSB/packages/xsbpy/examples/pong.py
+++
@@ -1,8 +0,0 @@
-import xsbpym
-
-def pong(N):
-    print("                   "+str(N)+': Python is better.')
-    N1 = N-1
-    String = "ping("+str(N1)+")."
-#    print(String)
-    d = xsbpym.querySingle(String)
