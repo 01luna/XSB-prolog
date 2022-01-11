b04f2a71fa92efa078cb6088bae09f392c68ca7d
Theresa Swift <tswift@bbn.com>
2022-01-11 13:25:26 -0500

    Forgot to commit these changes for px_comp.

--- XSB/packages/xsbpy/xsbpy_defs.h
+++ XSB/packages/xsbpy/xsbpy_defs.h
@@ -3,6 +3,7 @@
 #define PYOBJ_C "pyObj"
 #define PYDICT_C "pyDict"
 #define PYNONE_C "None"
+#define PROLOG_TERM_C "plgTerm"
 #define PYTUP_C  ""
 
 #define PYSET_PROLOG  pySet
@@ -10,4 +11,9 @@
 #define PYDICT_PROLOG pyDict
 #define PYNONE_PROLOG 'None'
 #define PYTUP_PROLOG  ''
+
+#define DELAY_LISTS 1
+#define NO_TRUTHVALS 2
+#define PLAIN_TRUTHVALS 4
+#define SET_COLLECTION 8
 
