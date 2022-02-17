cb545653c68b2b69c9d47128c740dfec326d682a
Theresa Swift <tswift@bbn.com>
2022-02-17 17:48:57 -0500

    Working towards a testsuite for px and xsbpy -- a way to go, but getting there...

--- xsbtests/python_tests/test.sh
+++ xsbtests/python_tests/test.sh
@@ -14,3 +14,11 @@ u=`uname`;
 #echo "uname for this system is $u";
 
 ../pygentest.sh $python px_callbacks.py
+../pygentest.sh $python test_cmd_query.py
+../pygentest.sh $python test_interrupts.py
+echo "Expect slight diffs in test_comps between (...) and (...), due to set creation in Python"
+../pygentest.sh $python test_comps.py
+
+#========== the following are non-regression tests ==========
+
+python3.8 test_iterations.py
